595678adb76f3946e513a41f3726c6fbab362996
Joao <joaovic2012@hotmail.com>
2022-12-31 19:03:13 -0300

    API viacep

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,4 +1,5 @@
 import Address from "../models/address.js";
+import * as addressService from "../services/address-service.js"
 
 function State() {
 
@@ -31,5 +32,67 @@ export function init() {
     state.errorCep = document.querySelector('[data-error="cep"]');
     state.errorNumber = document.querySelector('[data-error="number"]');
 
-    console.log(state);
+    state.inputNumber.addEventListener('change', handleInputNumberChange);
+    state.inputCep.addEventListener('change', handleInputCepChange);
+    state.btnClear.addEventListener('click', handleBtnClearClick);
+    state.btnSave.addEventListener('click', handleBtnSaveClick);
+
+}
+
+function handleInputNumberChange(event){
+    if(event.target.value == ""){
+        setFormError("number", "Campo requerido");
+    }
+    else{
+        setFormError("number", "");
+    }
+}
+
+async function handleInputCepChange(event){
+    const cep = event.target.value;
+
+    try {
+        const address = await addressService.findByCep(cep);
+
+        state.inputStreet.value = address.street;
+        state.inputCity.value = address.city;
+
+        state.address = address;
+        
+        setFormError("cep", "");
+
+        state.inputNumber.focus();
+    }
+    catch (e) {
+        setFormError("cep", "Informe um CEP válido");
+        document.forms.newAddress.street.value = '';
+        document.forms.newAddress.city.value = '';
+    }
+}
+
+function handleBtnClearClick(event){
+    event.preventDefault();
+    clearForm();
+}
+
+function clearForm(){
+    document.forms.newAddress.cep.value = '';
+    document.forms.newAddress.street.value = '';
+    document.forms.newAddress.number.value = '';
+    document.forms.newAddress.city.value = '';
+
+    setFormError("cep", "");
+    setFormError("number", "");
+
+    state.inputCep.focus();
+}
+
+async function handleBtnSaveClick(event){
+    event.preventDefault();
+    console.log(event.target);
+}
+
+function setFormError(key, value){
+    const element = document.querySelector(`[data-error="${key}"]`);
+    element.innerHTML = value;
 }
